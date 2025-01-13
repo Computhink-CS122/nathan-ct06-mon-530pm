a4b1e7c96706e40663158fa4deda269a8ad7d239
Computhink-CS122 <cs122@computhink.com.sg>
2025-01-13 10:13:11 +0000

    Jan 13, 2025, 10:13 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,4 +5,4 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
-# repeay 3 times
+# repeat 10 times 
