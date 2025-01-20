17275e5afa62da789aaa0c9094331fd01c3b45e7
Computhink-CS122 <cs122@computhink.com.sg>
2025-01-20 10:25:48 +0000

    Jan 20, 2025, 10:25 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -17,3 +17,20 @@ b = 2333
 print( a + b)
 
 
+
+
+
+
+
+
+
+
+# Subtract 10 from 50, then multiply the result by 3.
+
+# **Task 3c**:
+# Divide 100 by 4 and print the result.
+
+# **Task 3d**:
+# Calculate and print the result of (3 + 4) * 5 - (10 / 2).
+
+
