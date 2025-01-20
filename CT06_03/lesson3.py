b86a2585883a76af65b0bbb33842b9623dab4eb6
Computhink-CS122 <cs122@computhink.com.sg>
2025-01-20 09:43:40 +0000

    Jan 20, 2025, 9:43 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,3 +1,4 @@
 print("Hello from lesson 3")
-#make varble adam = 60 bradon = 58 charlie=98 daniel = 79 Add all the varible and divide by 4#
+#make varble adam = 60 bradon = 58 charlie=98 daniel = 79 Add all the varible and divide by 4
+
 
