46daaed25ef7e578f7a7c451bad7e0826ed29ea0
Computhink-CS122 <cs122@computhink.com.sg>
2025-01-20 10:49:42 +0000

    Jan 20, 2025, 10:49 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,13 +1,13 @@
 # print("Hello from lesson 4")
-na = input("what is ur name")
+na = input("what is ur name ")
 
 print(na)
 
-c = input("what is ur fav colour")
+c = input("what is ur fav colour ")
 
 print(c)
 
-age = input("what is ur age")
+age = input("what is ur age ")
 
 print(age)
 
